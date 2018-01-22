e161530ae34181b32c8da90d62995f1bd5513638
Yaksh Thakar <yakshthakar264@gmail.com>
2018-01-22 18:01:27 +0530

    Add files via upload

---
+++ face recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__made by:-"""Adam Geitgey"""
+__Updated by__ = """Yaksh Thakar"""
+__email__ = 'yakshthakar264@gmail.com'
+__version__ = '0.1.0'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
