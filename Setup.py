912b03681e8dc64a62385a47a07e11d1e70701c6
Yaksh Thakar <yakshthakar264@gmail.com>
2018-01-30 17:48:11 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -43,7 +43,7 @@ setup(
         ]
     },
     install_requires=requirements,
-    license="YBlog",
+    license="MIT license","LJIET"
     zip_safe=False,
     keywords='face_recognition',
     classifiers=[
